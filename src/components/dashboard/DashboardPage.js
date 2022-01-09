b8ad12b95914c076866121050c7df1030d35b0fd
benjaminykim <benkim96@gmail.com>
2022-01-08 17:19:11 -0800

    updates to content and dividends claim

--- src/components/dashboard/DashboardPage.js
+++ src/components/dashboard/DashboardPage.js
@@ -1,20 +1,20 @@
 import React from 'react';
 
 const DashboardPage = () => {
-  const cardStyle = "p-3 h-30 md:h-60 w-full min-h-24 flex flex-col flex-grow shadow bg-gray-800 rounded-lg text-slate-300"
-  const gridStyle = "grid content-start gap-2 xl:grid-cols-3 lg:grid-cols-2 md:grid-cols-1 text-slate-300"
+  const cardStyle = "p-3 h-30 md:h-60 w-full min-h-24 flex flex-col justify-between flex-grow shadow bg-gray-800 rounded-lg text-xl text-slate-300"
+  const gridStyle = "grid content-start gap-2 xl:grid-cols-3 lg:grid-cols-2 md:grid-cols-1 text-slate-300 text-center"
 
   return (
     <div class={gridStyle}>
-      <div class={cardStyle + ' justify-between space-y-4'}>
-        <p class="text-center text-xl text-slate-300">Assets Under Management</p>
-        <div className="align-self-center"><p className="text-4xl text-center text-white">$0</p></div>
-        <div className="align-self-end"><p className="align-self-end text-md text-slate-400">Development fund: $0</p></div>
+      <div class={cardStyle}>
+        <p>Assets Under Management</p>
+        <p className="text-4xl text-white">$0</p>
+        <div className="h-1"></div>
       </div>
-      <div class={cardStyle + ' px-4'}>
-        <p class="text-center text-xl text-slate-300">$INC Metrics</p>
-        <table class="table-auto mt-4">
-          <thead class="border-b-2 border-sky-200">
+      <div class={cardStyle}>
+        <p>$INC Metrics</p>
+        <table class="table-auto text-left text-md">
+          <thead class="border-b-2 border-gray-400">
             <tr className="text-gray-400">
               <th>METRIC</th>
               <th>VALUE</th>
--- src/components/dashboard/PortfolioPage.js
+++ src/components/dashboard/PortfolioPage.js
@@ -1,14 +1,17 @@
 import React from 'react';
 
 const PortfolioPage = () => {
-  const cardStyle = "p-3 h-30 md:h-60 w-full min-h-24 flex flex-col flex-grow shadow bg-gray-800 rounded-lg text-slate-300"
+  const cardStyle = "p-3 h-30 md:h-60 w-full min-h-24 flex flex-col justify-between flex-grow shadow bg-gray-800 rounded-lg text-xl text-slate-300"
+  const gridStyle = "grid content-start gap-2 xl:grid-cols-3 lg:grid-cols-2 md:grid-cols-1 text-slate-300 text-center"
+  // const cardStyle = "p-3 h-30 md:h-60 w-full min-h-24 flex flex-col flex-grow shadow bg-gray-800 rounded-lg text-slate-300"
 
   return (
-    <div class="grid content-start gap-2 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-1">
-      <div class="flex flex-col p-3 h-30 mb-2 md:h-60 min-h-24 shadow bg-gray-800 text-slate-300 rounded-lg">
-        <p class="text-left text-xl">Portfolio Overview</p>
-        <table class="table-auto mt-4">
-          <thead class="border-b-2 border-sky-200">
+    <div class="grid content-start gap-2 text-center text-white xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-1">
+      {/* <div class="flex flex-col p-3 h-30 mb-2 md:h-60 min-h-24 shadow bg-gray-800 rounded-lg"> */}
+      <div class={cardStyle}>
+        <p class="text-xl">Portfolio Overview</p>
+        <table class="table-auto text-left text-md">
+          <thead class="border-b-2 border-gray-400">
             <tr className="text-gray-400">
               <th>METRIC</th>
               <th>VALUE</th>
@@ -38,17 +41,12 @@ const PortfolioPage = () => {
           </tbody>
         </table>
       </div>
-      <div class={cardStyle + ' justify-between space-y-4'}>
-        <p class="text-left text-xl">Pending Dividends</p>
-        <p className="align-self-center text-4xl text-center text-white">0 ETH / 0 INC</p>
-        <div class="flex flex-row justify-around mb-2">
-          <button className="transition ease-in-out hover:scale-110 duration-300 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border-0 rounded-md shadow-sm text-base font-medium text-white bg-blue-600 hover:bg-blue-700">
-            Claim as ETH
-          </button>
-          <button className="transition ease-in-out hover:scale-110 duration-300 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border-0 rounded-md shadow-sm text-base font-medium text-white bg-blue-600 hover:bg-blue-700">
-            Claim as INC
-          </button>
-        </div>
+      <div class={cardStyle + ' space-y-4'}>
+        <p class="text-xl">Pending Dividends</p>
+        <p className="text-4xl">0 ETH</p>
+        <button className="align-self-center transition ease-in-out hover:scale-110 duration-300 whitespace-nowrap inline-flex justify-center px-4 py-2 border-0 rounded-md shadow-sm text-base font-medium text-white bg-blue-600 hover:bg-blue-700">
+          Claim as ETH
+        </button>
       </div>
     </div>
   );
